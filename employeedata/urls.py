9364954b4973cc35009a6e0908ee60adb5f25c76
shubham4576 <shubhamlaxkar851@gmail.com>
2024-05-09 17:42:17 +0530

    Changes for Hosting on vercel

--- employeedata/urls.py
+++ employeedata/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', include('employee_details.urls')),
 ]
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
